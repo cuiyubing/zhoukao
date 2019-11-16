5079c0943dbdbd2079716a23dcb9969b30c1f3fc
bingbing <1565570355@qq.com>
2019-11-16 17:38:51 +0800

    修改提交

---
+++ pro-service/src/main/java/com/bw/dao/ProductInfoDao.java
@@ -0,0 +1,9 @@
+package com.bw.dao;
+
+import com.bw.entity.ProductCategory;
+import com.bw.entity.ProductInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface ProductInfoDao extends JpaRepository<ProductCategory,String>, JpaSpecificationExecutor<ProductCategory> {
+}
